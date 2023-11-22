cac0929f58abfae676ece0e797c2f856b3bdd532
resty <grande.resty@gmail.com>
2023-11-22 16:08:27 +0800

    feat(backend): add catalogue

--- backend-server/src/app.module.ts
+++ backend-server/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CataloguesModule } from './catalogues/catalogues.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.production.env'
+          : '.development.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    CataloguesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
