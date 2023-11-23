2fe20163c03274ec17cd55e23bec1871f41acd29
cloi <mejaricruz123@gmail.com>
2023-11-23 16:45:58 +0800

    fix(backend): port

--- backend-server/src/main.ts
+++ backend-server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  const port = configService.get<number>('PORT', 3000);
+  const port = configService.get<number>('PORT', 4001);
   app.enableCors();
   await app.listen(port);
 }
